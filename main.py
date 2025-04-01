68eb541809d3b23fedf39da4de350e22d6309fb4
Kashiwagi Maya <maya@KashiwaginoMacBook-Pro.local>
2025-04-01 17:27:53 +0900

    Supabase対応のmain.pyに修正

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app = FastAPI()
 # CORS設定（Reactアプリと連携）
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://morning-check-app.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
